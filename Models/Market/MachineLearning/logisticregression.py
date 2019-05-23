e9d4c8b56794e5b3bfd1f8fddd6dbe613dca97df
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-05-23 16:51:14 +0200

    logreg_market_example

--- Models/Market/MachineLearning/logisticregression.py
+++ Models/Market/MachineLearning/logisticregression.py
@@ -1,18 +1,35 @@
 
 from sklearn.linear_model import LogisticRegression
 import pandas
+import numpy
 
 d = pandas.read_csv('data/training.csv')
-dp = pandas.read_csv('data/predict.csv')
-X = d[['density','resistance']]
-y = d['burnt']
+dp = pandas.read_csv('data/validation.csv')
 
+X = d[['density','resistance']]
+y = d['binaryburnt']
+#print(X)
+
+Xp = dp[['density','resistance']]
+yp = dp['binaryburnt']
 
 clf = LogisticRegression(random_state=0, solver='lbfgs').fit(X, y)
 
-clf.predict(Xp,yp)
+pred = clf.predict(Xp)
 
-clf.score(Xp,yp)
+#inds = (numpy.where(abs(pred - yp)==1))[0].tolist()
+#print(inds)
+
+prederror = dp.loc[abs(pred - yp)==1]
+
+print(prederror['density'])
+print(prederror['resistance'])
+
+errdensity = prederror['density']
+errresistance = prederror['resistance']
+
+score = clf.score(Xp,yp)
+print(score)
 
 
 
